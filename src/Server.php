2206a9e66e667dc66ce10a6eca1313565d80609d
李铭昕 <715557344@qq.com>
2021-07-07 14:54:45 +0800

    Format code

--- src/Server.php
+++ src/Server.php
@@ -1,10 +1,14 @@
 <?php
-/**
- * User: lufei
- * Date: 2021/4/21
- * Email: lufei@swoole.com
- */
 
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
 namespace Hyperf\MqttServer;
 
 use Hyperf\Contract\ConfigInterface;
